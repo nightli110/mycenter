d9ebfaf5ec992493495d7ccbe0ba350c1535bcf2
nightli <1658484908@qq.com>
2020-12-12 22:36:05 +0800

    center 有问题

--- main.cpp
+++ main.cpp
@@ -27,5 +27,129 @@ using HttpClient = SimpleWeb::Client<SimpleWeb::HTTP>;
 int main()
 {
     PrintMonster();
-    return 1;
+    HttpServer server;
+    server.config.port = 8080;
+
+    MyDB db;
+    InferenceAPPMap mytest;
+    MYSQL_RES *result;
+    db.initDB("localhost", "root", "123456", "app");
+
+    mytest.InitDB(&db);
+
+    // MyCenter *CeneterView;
+    shared_ptr<MyAPPActionView> AppActionView = make_shared<MyAPPActionView>(&mytest);
+    server.resource["^/helloworld"]["GET"]=[](shared_ptr<HttpServer::Response> response, shared_ptr<HttpServer::Request> request){
+        try
+        {
+            string response_str="helloworld";
+            responsr->write(response_str);
+        }
+        catch (const exception &e)
+        {
+            *response << "HTTP/1.1 400 Bad Request\r\nContent-Length: " << strlen(e.what()) << "\r\n\r\n"
+                      << e.what();
+        }
+    };
+
+    server.resource["^/inference/register"]["POST"] = [AppActionView](shared_ptr<HttpServer::Response> response, shared_ptr<HttpServer::Request> request) {
+        try
+        {
+            string request_str = request->content.string();
+
+            string response_str = AppActionView->AppRegister(request_str);
+
+            response->write(response_str);
+        }
+        catch (const exception &e)
+        {
+            *response << "HTTP/1.1 400 Bad Request\r\nContent-Length: " << strlen(e.what()) << "\r\n\r\n"
+                      << e.what();
+        }
+    };
+
+    server.resource["^/inference/unregister"]["POST"] = [AppActionView](shared_ptr<HttpServer::Response> response, shared_ptr<HttpServer::Request> request) {
+        try
+        {
+            /* code */
+            string request_str = request->content.string();
+            string response_str = AppActionView->AppUnRegister(request_str);
+            response->write(response_str);
+        }
+        catch (const std::exception &e)
+        {
+            *response << "HTTP/1.1 400 Bad Request\r\nContent-Length: " << strlen(e.what()) << "\r\n\r\n"
+                      << e.what();
+        }
+    };
+
+    server.resource["^/inference/online"]["POST"] = [AppActionView](shared_ptr<HttpServer::Response> response, shared_ptr<HttpServer::Request> request) {
+        try
+        {
+            /* code */
+            string request_str = request->content.string();
+            string response_str = AppActionView->AppOnline(request_str);
+            response->write(response_str);
+        }
+        catch (const std::exception &e)
+        {
+            *response << "HTTP/1.1 400 Bad Request\r\nContent-Length: " << strlen(e.what()) << "\r\n\r\n"
+                      << e.what();
+        }
+    };
+
+    server.resource["^/inference/offline"]["POST"] = [AppActionView](shared_ptr<HttpServer::Response> response, shared_ptr<HttpServer::Request> request) {
+        try
+        {
+            /* code */
+            string request_str = request->content.string();
+            string response_str = AppActionView->AppOffLine(request_str);
+            response->write(response_str);
+        }
+        catch (const std::exception &e)
+        {
+            *response << "HTTP/1.1 400 Bad Request\r\nContent-Length: " << strlen(e.what()) << "\r\n\r\n"
+                      << e.what();
+        }
+    };
+
+    server.resource["^/infomap"]["GET"] = [AppActionView](shared_ptr<HttpServer::Response> response, shared_ptr<HttpServer::Request> request) {
+        try
+        {
+            string response_str = AppActionView->GetMap();
+            response->write(response_str);
+        }
+        catch (const std::exception &e)
+        {
+            std::cerr << e.what() << '\n';
+        }
+
+        response->write(request->path_match[1].str());
+    };
+
+    // server.resource["^/omsdata"]["POST"] = [&CeneterView](shared_ptr<HttpServer::Response>, shared_ptr<HttpServer::Request> request)
+    // {
+    //     try
+    //     {
+    //         string request_str = request->content.string();
+    //         Json::Value request_json = StringToJson(request_str);
+                                                                                                                                     
+
+    //     }
+    //     catch(const std::exception& e)
+    //     {
+    //         std::cerr << e.what() << '\n';
+    //     }
+        
+    // };
+
+    promise<unsigned short> server_port;
+    thread server_thread([&server, &server_port]() {
+        // Start server
+        server.start([&server_port](unsigned short port) {
+            server_port.set_value(port);
+        });
+    });
+    server_thread.join();
+    // return 1;
 }
